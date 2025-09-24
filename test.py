48f11257cc6ef48ffb96cb96d071cd3189b42ed7
Eric Tsai <eric1214t@gmail.com>
2025-09-24 14:30:56 +0800

    chang test.py to trigger CI

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 # hello.py
 def foo():
-    print("Hello")
-    try:
-        x = 9/0
-    except:
-        x = None
-    return "ok"
+	print("Hello")
+	try:
+		x = 9/0
+	except:
+		x = None
+	return "ok"
 # x=58/0
 # pytest 測試函式
 def test_foo():
-    assert foo() == "ok"
+	assert foo() == "ok"
