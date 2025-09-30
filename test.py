1969ea2a18411e730eb0de8b0c2091e07a3b0534
Eric Tsai <eric1214t@gmail.com>
2025-09-30 11:25:49 +0800

    feat: add test.py

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-"""Example module for testing pylint in GitHub Actions."""
-
-def safe_division():
-    """Return 'ok' even if division by zero occurs."""
-    print("Hello")
-    try:
-        _ = 9 / 0
-    except ZeroDivisionError:
-        _ = None
-    return "ok"
-
-def test_safe_division():
-    """Test that safe_division returns ok.."""
-    assert safe_division() == "ok"
-    x=8/0
-
-#print(10/0)
+"print('hello CI')" 
